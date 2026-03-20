0e2644fd7a36bc51055533f73c9756077b07ae9a
LorjinandLolo <131661447+LorjinandLolo@users.noreply.github.com>
2026-03-20 19:18:58 +0100

    varcel sucks
    i HATE THIS SHIT.

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,32 @@
-self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-5c1d73b22852189c.js","static/chunks/fd9d1056-956c49351650e0a5.js","static/chunks/117-59afa68732cda1c8.js","static/chunks/main-app-fba57115bb70e587.js"],pages:{"/_app":["static/chunks/webpack-5c1d73b22852189c.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-fc986461814f3306.js","static/chunks/pages/_app-ec50c177bb353606.js"],"/_error":["static/chunks/webpack-5c1d73b22852189c.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-fc986461814f3306.js","static/chunks/pages/_error-4d91465942d9858e.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+self.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [
+    "static/chunks/react-refresh.js"
+  ],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack.js",
+    "static/chunks/main-app.js"
+  ],
+  "pages": {
+    "/_app": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/_app.js"
+    ],
+    "/_error": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/_error.js"
+    ]
+  },
+  "ampFirstPages": []
+};
+self.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
